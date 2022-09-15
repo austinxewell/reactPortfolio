69f96467cbd63ddd83e5343df66f18a28049d7dc
AustinxEwell <austin.ewell86@gmail.com>
2022-09-14 19:48:13 -0600

    created started files and basic pages for organization. installed react router and have the pages rendering based on URL

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+// Component Imports
+import Nav from './components/Nav'
+import LandingPage from './components/LandingPage'
+import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
+import Resume from './components/Resume'
+
+
+function App() {
+  const [ currentPage, handlePageChange] = useState('LandingPage')
+
+  const renderPage = (currentPage) => {
+    switch(currentPage) {
+      case 'LandingPage':
+        return <LandingPage />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Resume':
+        return <Resume />;
+    }
+  }
+
+  return (
+    <body>
+      <Router>
+        <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        <div>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/portfolio' element={<Portfolio />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/resume' element={<Resume />} />
+
+            <Route element={<LandingPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </body>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LandingPage = () => {
+    return(
+        <div>
+            <h1>--Landing--</h1>
+            <p>Hello there!</p>
+        </div>
+    )
+}
+
+export default LandingPage;
