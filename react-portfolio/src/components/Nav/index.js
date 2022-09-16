acefb5f9666a9ab999646fa82f84e26955d387c9
AustinxEwell <austin.ewell86@gmail.com>
2022-09-16 01:45:04 -0600

    minor css changes to nav links and nav header

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import styles from './nav.css'
 
 // Imported Files
 import miniLogo from "../../assets/logos/miniLogo.JPG" 
@@ -9,34 +8,39 @@ const Nav = () => {
     const navLinkStylesAbout = ({ isActive }) => {
         return {
             color: isActive ? 'red' : 'black',
-            fontSize: isActive ? '50px' : '40px'
+            fontSize: isActive ? '50px' : '40px',
+            fontWeight: isActive ? 'bold' : 'normal'
         }
     }
     const navLinkStylesPortfolio = ({ isActive }) => {
         return {
             color: isActive ? 'blue' : 'black',
-            fontSize: isActive ? '50px' : '40px'
+            fontSize: isActive ? '50px' : '40px',
+            fontWeight: isActive ? 'bold' : 'normal'
         }
     }
     const navLinkStylesContact = ({ isActive }) => {
         return {
             color: isActive ? 'yellow' : 'black',
-            fontSize: isActive ? '50px' : '40px'
+            fontSize: isActive ? '50px' : '40px',
+            fontWeight: isActive ? 'bold' : 'normal'
         }
     }
     const navLinkStylesResume = ({ isActive }) => {
         return {
             color: isActive ? 'green' : 'black',
-            fontSize: isActive ? '50px' : '40px'
+            fontSize: isActive ? '50px' : '40px',
+            fontWeight: isActive ? 'bold' : 'normal'
         }
     }
 
-
     return(
         <div className='bg-gray-700 py-4 flex'>
-            <img src={miniLogo} className="rounded w-16 ml-4"></img>
-            <p className='text-3xl text-pink-500'>Austin Ewell</p>
-            <ul className='absolute right-12 text-right'>
+            <div className='w-1/12'>
+                <img src={miniLogo} className="rounded w-16 ml-4"></img>
+            </div>
+            <p className='text-5xl text-white mx-auto w-5/12'>Austin Ewell</p>
+            <ul className='absolute right-12 text-right w-4/12'>
                 <li>
                     <NavLink end to="/" style={navLinkStylesAbout}>About</NavLink>
                 </li>
