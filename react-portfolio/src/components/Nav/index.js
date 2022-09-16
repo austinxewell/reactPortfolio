eb90e7f7f358200d7f9f88ba10fe27b4ec462cdd
AustinxEwell <austin.ewell86@gmail.com>
2022-09-16 01:27:37 -0600

    navigation basic set up complete. waiting on color schemes before making updates.

--- react-portfolio/src/components/Nav/index.js
+++ react-portfolio/src/components/Nav/index.js
@@ -6,25 +6,28 @@ import styles from './nav.css'
 import miniLogo from "../../assets/logos/miniLogo.JPG" 
 
 const Nav = () => {
-
     const navLinkStylesAbout = ({ isActive }) => {
         return {
-            color: isActive ? 'red' : 'white'
+            color: isActive ? 'red' : 'black',
+            fontSize: isActive ? '50px' : '40px'
         }
     }
     const navLinkStylesPortfolio = ({ isActive }) => {
         return {
-            color: isActive ? 'blue' : 'white'
+            color: isActive ? 'blue' : 'black',
+            fontSize: isActive ? '50px' : '40px'
         }
     }
     const navLinkStylesContact = ({ isActive }) => {
         return {
-            color: isActive ? 'yellow' : 'white'
+            color: isActive ? 'yellow' : 'black',
+            fontSize: isActive ? '50px' : '40px'
         }
     }
     const navLinkStylesResume = ({ isActive }) => {
         return {
-            color: isActive ? 'green' : 'white'
+            color: isActive ? 'green' : 'black',
+            fontSize: isActive ? '50px' : '40px'
         }
     }
 
@@ -33,18 +36,18 @@ const Nav = () => {
         <div className='bg-gray-700 py-4 flex'>
             <img src={miniLogo} className="rounded w-16 ml-4"></img>
             <p className='text-3xl text-pink-500'>Austin Ewell</p>
-            <ul className='absolute right-4'>
+            <ul className='absolute right-12 text-right'>
                 <li>
-                    <NavLink end to="/" style={navLinkStylesAbout} className='text-3xl'>About</NavLink>
+                    <NavLink end to="/" style={navLinkStylesAbout}>About</NavLink>
                 </li>
                 <li>
-                    <NavLink to="Portfolio" style={navLinkStylesPortfolio} className='text-3xl'>Portfolio</NavLink>
+                    <NavLink to="Portfolio" style={navLinkStylesPortfolio}>Portfolio</NavLink>
                 </li>
                 <li>
-                    <NavLink to="Contact" style={navLinkStylesContact} className='text-3xl'>Contact</NavLink>
+                    <NavLink to="Contact" style={navLinkStylesContact}>Contact</NavLink>
                 </li>
                 <li>
-                    <NavLink to="Resume" style={navLinkStylesResume} className='text-3xl'>Resume</NavLink>
+                    <NavLink to="Resume" style={navLinkStylesResume}>Resume</NavLink>
                 </li>
             </ul>
         </div>
